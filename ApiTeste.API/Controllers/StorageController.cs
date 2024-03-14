fa4feb4f2f58af5708d620601180868e9a529d8e
CassioJhones <cassio.bjhones@gmail.com>
2024-03-13 22:24:10 -0300

    Adicionar arquivos de projeto.

---
+++ ApiTeste.API/Controllers/StorageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTeste.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult UploadImage(IFormFile arquivo)
+    {
+        return Created();
+    }
+}
