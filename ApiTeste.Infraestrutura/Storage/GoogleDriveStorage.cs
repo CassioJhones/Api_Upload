a360e74305c99e28a434d7e5837486e82183873c
CassioJhones <cassio.bjhones@gmail.com>
2024-03-14 01:51:34 -0300

    validacao dos status

--- ApiTeste.Infraestrutura/Storage/GoogleDriveStorage.cs
+++ ApiTeste.Infraestrutura/Storage/GoogleDriveStorage.cs
@@ -1,5 +1,6 @@
 ﻿using ApiTeste.Domain.Entidades;
 using ApiTeste.Domain.Storage;
+using Google.Apis.Drive.v3;
 using Microsoft.AspNetCore.Http;
 
 namespace ApiTeste.Infraestrutura.Storage;
@@ -7,6 +8,19 @@ public class GoogleDriveStorage : IStorageService
 {
     public string Upload(IFormFile arquivo, User user)
     {
+        DriveService servicoGoogle = new();
 
-    } 
+        Google.Apis.Drive.v3.Data.File arquivoDrive = new()
+        {
+            Name = arquivo.Name,
+            MimeType = arquivo.ContentType
+        };
+        FilesResource.CreateMediaUpload comando = servicoGoogle.Files.Create(arquivoDrive, arquivo.OpenReadStream(), arquivo.ContentType);
+
+        Google.Apis.Upload.IUploadProgress resultado = comando.Upload();
+
+        if (resultado.Status is not Google.Apis.Upload.UploadStatus.Completed
+            or Google.Apis.Upload.UploadStatus.NotStarted)
+            throw resultado.Exception;
+    }
 }
